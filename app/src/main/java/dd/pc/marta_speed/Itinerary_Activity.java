bee322fdeb41e96a521b0937fabdd0c006664c85
Notorious <noto.scott1@gmail.com>
2017-09-06 20:56:38 -0400

    Initial commit

---
+++ app/src/main/java/dd/pc/marta_speed/Itinerary_Activity.java
@@ -0,0 +1,13 @@
+package dd.pc.marta_speed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Itinerary_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary_);
+    }
+}
